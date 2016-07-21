dbd2dc84cf69569f0c429408f4e7358e8b1f5349
S. Scalice <sscalice92@gmail.com>
2016-07-20 17:04:15 -0700

    merge

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -283,7 +283,7 @@ function main(){
 						else if(numServs) service += " + " + numServs + " others";
 						var total = appointment[5];
 						var notes = appointment[8] ? appointment[8] : "";
-						var newNode = $('<tr apptId="' + appointment[0]+ '"class="highlight clearboth" data-toggle="modal" data-id="1" data-target="#apptModal"><td>'+ date +'</td><td>'+ service +'</td><td>$'+ total +'</td><td>'+ notes +'</td></tr>');
+						var newNode = $('<tr apptId="' + appointment[0]+ '"class="highlight clearboth" data-toggle="modal" data-target="#apptModal"><td class="200">'+ date +'</td><td class="250">'+ service +'</td><td class="small">$'+ total +'</td><td class="200">'+ notes +'</td><td class="col100 center print"><span id="print" class="icon icon-print"></span></td></tr>');
 						$('#appointments').append(newNode);
 						newNode.click(setCurrentAppointmentId);
 					}
